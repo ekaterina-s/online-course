a7fd369d97a7c079dab65556bed7b79f1efbf305
ekaterina-s <catshavrina@gmail.com>
2022-03-06 17:19:32 +0100

    закончила низ футера, начала верстать основную часть

--- app/js/main.js
+++ app/js/main.js
@@ -1,2 +1,13 @@
-import {testFunction} from './scripts/test';
+import { testFunction } from './scripts/test';
 testFunction();
+
+const burger = document.querySelector('.js-burger');
+console.log(burger);
+const nav = document.querySelector('.js-nav');
+const body = document.querySelector('.js-body');
+
+burger.addEventListener('click', () => {
+  burger.classList.toggle('_active');
+  nav.classList.toggle('_active');
+  body.classList.toggle('lock');
+})
